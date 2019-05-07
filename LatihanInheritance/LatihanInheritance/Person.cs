272ea0db735be5bbe79a698053ea824eed1cae4a
anggzh <45726270+anggzh@users.noreply.github.com>
2019-05-07 14:53:17 +0700

    Add files via upload

---
+++ LatihanInheritance/LatihanInheritance/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void GetNameAndAge()
+        {
+            Console.WriteLine("The person has name : {0} and age {1}", Name, Age);
+        }
+
+        public class Teacher : Person
+        {
+            public string TeacherId { get; set; }
+            public string Subject { get; set; }
+        }
+
+        public class Student : Person
+        {
+            public string StudentId { get; set; }
+            public string Email { get; set; }
+        }
+
+
+    }
+
+}
---
+++ LatihanInheritance/LatihanInheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Mike";
+            person.Age = 20;
+            person.GetNameAndAge();
+
+            Person.Teacher teacher = new Person.Teacher();
+            teacher.Name = "Thomas ";
+            teacher.Age = 35;
+            teacher.TeacherId = "190302";
+            teacher.Subject = "Math";
+            teacher.GetNameAndAge();
+
+            Person.Student student = new Person.Student();
+            student.Name = "Sara";
+            student.Age = 19;
+            student.StudentId = "10506";
+            student.Email = "sara1234@amikom.ac.id";
+            student.GetNameAndAge();
+
+
+            Console.ReadKey();
+        }
+    }
+}
